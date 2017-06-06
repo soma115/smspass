d702dec802da59483f685e362ed4844447a8c80e
rfialek <rfialek@pronet.pl>
2017-06-06 13:09:38 +0200

    second

--- install.sh
+++ install.sh
@@ -6,20 +6,20 @@ rpm -ivh epel-release-7-9.noarch.rpm
 
 if yum -y install httpd wget gcc intltool spice-glib-devel gnokii
 then
-  echo ">>> Zainstalowałem: httpd wget gcc intltool spice-glib-devel gnokii <<<"
+    echo "iptables -I INPUT -p tcp --dport 80 -j ACCEPT" >> /etc/rc.local
+    iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+    chmod +x /etc/rc.d/rc.local
+
+    mkdir -p /opt/bin/smspass
+    cp ./smspass.py /opt/bin/smspass
+    cp ./smspass.db /opt/bin/smspass
+    echo "/opt/bin/smspass/smspass.py &" >> /etc/rc.local
+    sed -i 's/port = none/port = \/dev\/ttyUSB2/g' /etc/gnokiirc
+    sed -i 's/model = fake/model = AT/g' /etc/gnokiirc
+    sed -i 's/use_locking = yes/use_locking = no/g' /etc/gnokiirc
+    echo ">>> Wszystko poszlo dobrze <<<"
 else
-  echo ">>> Problem z instalacją: httpd wget gcc intltool spice-glib-devel gnokii <<<"
+  echo ">>> Problem z instalacja <<<"
 fi
 
-echo "iptables -I INPUT -p tcp --dport 80 -j ACCEPT" >> /etc/rc.local
-iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-chmod +x /etc/rc.d/rc.local
-
-mkdir -p /opt/bin/smspass
-cp ./smspass.py /opt/bin/smspass
-cp ./smspass.db /opt/bin/smspass
-echo `pwd`"/smspass.py &" >> /etc/rc.local
-sed -i 's/port = none/port = \/dev\/ttyUSB0/g' /etc/gnokiirc
-sed -i 's/model = fake/model = AT/g' /etc/gnokiirc
-sed -i 's/use_locking = yes/use_locking = no/g' /etc/gnokiirc
 
