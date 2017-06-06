408d0ffbb7db67aa4c798aa7924244300b38df0e
rfialek <rfialek@pronet.pl>
2017-06-06 11:55:32 +0200

    second

--- install.sh
+++ install.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
-if yum -y install httpd wget gcc intltool spice-glib-devel
-then
-  echo ">>> Zainstalowałem: httpd wget gcc intltool spice-glib-devel <<<"
-else
-  echo ">>> Problem z instalacją: httpd wget gcc intltool spice-glib-devel <<<"
-fi
+# Dodaj EPEL repo aby zainstalowac Gnokii:
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+rpm -ivh epel-release-7-9.noarch.rpm
 
-if yum -y install gnokii
+if yum -y install httpd wget gcc intltool spice-glib-devel gnokii
 then
-  echo ">>> Udało się <<<"
+  echo ">>> Zainstalowałem: httpd wget gcc intltool spice-glib-devel gnokii <<<"
 else
-  echo ">>> Muszę skompilować gnokii. Poczekaj minutkę. <<<"
-  yum -y install httpd wget gcc intltool spice-glib-devel ; cd
-  wget https://www.gnokii.org/download/gnokii/0.6.x/gnokii-0.6.31.tar.gz
-  tar -xzf ./gnokii-0.6.31.tar.gz ; cd gnokii-0.6.31 ; ./configure ; gmake ; gmake install
+  echo ">>> Problem z instalacją: httpd wget gcc intltool spice-glib-devel gnokii <<<"
 fi
 
 echo "iptables -I INPUT -p tcp --dport 80 -j ACCEPT" >> /etc/rc.local
@@ -22,7 +16,8 @@ iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 chmod +x /etc/rc.d/rc.local
 
 mkdir -p /opt/bin/smspass
-cp ./* /opt/bin/smspass
+cp ./smspass.py /opt/bin/smspass
+cp ./smspass.db /opt/bin/smspass
 echo `pwd`"/smspass.py &" >> /etc/rc.local
 sed -i 's/port = none/port = \/dev\/ttyUSB0/g' /etc/gnokiirc
 sed -i 's/model = fake/model = AT/g' /etc/gnokiirc
